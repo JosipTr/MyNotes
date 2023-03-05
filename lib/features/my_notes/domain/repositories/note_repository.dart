805b2617479f2c8946d6c533e3dac6894ee8a0fe
JosipTr <jtr.other13@gmail.com>
2023-03-05 13:33:51 +0100

    Create NoteRepository abstract class

---
+++ lib/features/my_notes/domain/repositories/note_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_notes/features/my_notes/domain/entities/note.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class NoteRepository {
+  Future<Either<Failure, List<Note>>> getAllNotes();
+  Future<Either<Failure, void>> insertNote(Note note);
+  Future<Either<Failure, void>> removeNote(Note note);
+  Future<Either<Failure, void>> updateNote();
+}
