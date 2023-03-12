c09c27fcf64c16023605c29c465d897c2cdbf898
JosipTr <jtr.other13@gmail.com>
2023-03-12 19:09:51 +0100

    Update menu

--- lib/features/my_notes/presentation/widgets/menu.dart
+++ lib/features/my_notes/presentation/widgets/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_notes/core/colors/generate_material_color.dart';
 import 'package:flutter_notes/features/my_notes/presentation/bloc/note_bloc.dart';
 import 'package:flutter_notes/features/my_notes/presentation/bloc/note_event.dart';
 import 'package:go_router/go_router.dart';
@@ -11,19 +12,30 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
-        color: Theme.of(context).scaffoldBackgroundColor,
+        color: Theme.of(context).cardColor,
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              child: const Text('Drawer Header'),
-            ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/notebook_header.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                )),
             ListTile(
-              leading: const Icon(Icons.note),
-              title: const Text('All notes'),
+              leading: const Icon(
+                Icons.note,
+                color: labelColor,
+              ),
+              title: Text(
+                'All notes',
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
               onTap: () {
                 context.read<NoteBloc>().add(const GetAllNotesEvent());
                 Navigator.of(context).pop();
@@ -31,8 +43,14 @@ class Menu extends StatelessWidget {
               },
             ),
             ListTile(
-              leading: const Icon(Icons.delete),
-              title: const Text('Trash'),
+              leading: const Icon(
+                Icons.delete,
+                color: labelColor,
+              ),
+              title: Text(
+                'Trash',
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
               onTap: () {
                 Navigator.of(context).pop();
                 context
@@ -42,8 +60,14 @@ class Menu extends StatelessWidget {
               },
             ),
             ListTile(
-              leading: const Icon(Icons.folder_special),
-              title: const Text('Categorized'),
+              leading: const Icon(
+                Icons.folder_special,
+                color: labelColor,
+              ),
+              title: Text(
+                'Categorized',
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
               onTap: () {
                 // Update the state of the app.
                 // ...
