d7c50fef318c6944a5ba166050d9fd660d4f422b
JosipTr <jtr.other13@gmail.com>
2023-03-15 19:22:13 +0100

    Add throw statements

--- lib/features/my_notes/data/datasources/local_data_sources/note_local_data_source.dart
+++ lib/features/my_notes/data/datasources/local_data_sources/note_local_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_notes/core/errors/exception.dart';
+
 import '../../models/note_model.dart';
 import '../../models/sort_model.dart';
 import 'database/database.dart';
@@ -44,90 +46,154 @@ class NoteLocalDataSourceImpl implements NoteLocalDataSource {
 
   @override
   Future<List<NoteModel>> getNotes(String sortType) async {
-    final notes = await _appDatabase.noteDao.getNotes(sortType);
-    return notes.map((note) => NoteModel.fromNote(note)).toList();
-  }
-
-  @override
-  Future<List<NoteModel>> getDeletedNotes(String sortType) async {
-    final notes = await _appDatabase.noteDao.getDeletedNotes(sortType);
-    return notes.map((note) => NoteModel.fromNote(note)).toList();
-  }
-
-  @override
-  Future<List<NoteModel>> getSearchedNotes(String searchValue) async {
-    final notes = await _appDatabase.noteDao.getSearchedNotes(searchValue);
-    return notes.map((note) => NoteModel.fromNote(note)).toList();
-  }
-
-  @override
-  Future<List<NoteModel>> getFavoriteNotes(String sortType) async {
-    final notes = await _appDatabase.noteDao.getFavoriteNotes(sortType);
-    return notes.map((note) => NoteModel.fromNote(note)).toList();
-  }
-
-  @override
-  Future<void> insertNote(NoteModel noteModel) {
-    return _appDatabase.noteDao.insertNote(noteModel);
-  }
-
-  @override
-  Future<void> updateNoteContent(NoteModel noteModel) {
-    return _appDatabase.noteDao.updateNoteContent(noteModel);
-  }
-
-  @override
-  Future<void> updateSortType(String sortType) {
-    return _appDatabase.sortDao.updateSortType(sortType);
-  }
-
-  @override
-  Future<void> removeNotes() {
-    return _appDatabase.noteDao.removeNotes();
-  }
-
-  @override
-  Future<void> selectAllNotes() {
-    return _appDatabase.noteDao.selectAllNotes();
-  }
-
-  @override
-  Future<void> selectNote(int id) {
-    return _appDatabase.noteDao.selectNote(id);
-  }
-
-  @override
-  Future<void> setNoteDeleted() {
-    return _appDatabase.noteDao.setNoteDeleted();
-  }
-
-  @override
-  Future<void> setNoteUndeleted() {
-    return _appDatabase.noteDao.setNoteUndeleted();
-  }
-
-  @override
-  Future<void> unselectAllNotes() {
-    return _appDatabase.noteDao.unselectAllNotes();
-  }
-
-  @override
-  Future<String?> getSortType() async {
-    if (await _appDatabase.sortDao.getSortType() != null) {
-      return _appDatabase.sortDao.getSortType();
-    } else {
-      insertSort(SortModel(id: 1, sortType: 'title'));
-      return _appDatabase.sortDao.getSortType();
+    try {
+      final notes = await _appDatabase.noteDao.getNotes(sortType);
+      return notes.map((note) => NoteModel.fromNote(note)).toList();
+    } catch (e) {
+      throw DatabaseException();
     }
   }
 
   @override
-  Future<void> insertSort(SortModel sortModel) {
-    return _appDatabase.sortDao.insertSort(sortModel);
+  Future<List<NoteModel>> getDeletedNotes(String sortType) async {
+    try {
+      final notes = await _appDatabase.noteDao.getDeletedNotes(sortType);
+      return notes.map((note) => NoteModel.fromNote(note)).toList();
+    } catch (e) {
+      throw DatabaseException();
+    }
   }
 
   @override
-  Future<void> selectFavoriteNote(int id) {
-    return _appDatabase.noteDao.selectFavoriteNote(id);
+  Future<List<NoteModel>> getSearchedNotes(String searchValue) async {
+    try {
+      final notes = await _appDatabase.noteDao.getSearchedNotes(searchValue);
+      return notes.map((note) => NoteModel.fromNote(note)).toList();
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<List<NoteModel>> getFavoriteNotes(String sortType) async {
+    try {
+      final notes = await _appDatabase.noteDao.getFavoriteNotes(sortType);
+      return notes.map((note) => NoteModel.fromNote(note)).toList();
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> insertNote(NoteModel noteModel) async {
+    try {
+      return await _appDatabase.noteDao.insertNote(noteModel);
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> updateNoteContent(NoteModel noteModel) async {
+    try {
+      return await _appDatabase.noteDao.updateNoteContent(noteModel);
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> updateSortType(String sortType) async {
+    try {
+      return await _appDatabase.sortDao.updateSortType(sortType);
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> removeNotes() async {
+    try {
+      return await _appDatabase.noteDao.removeNotes();
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> selectAllNotes() async {
+    try {
+      return await _appDatabase.noteDao.selectAllNotes();
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> selectNote(int id) async {
+    try {
+      return await _appDatabase.noteDao.selectNote(id);
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> setNoteDeleted() async {
+    try {
+      return await _appDatabase.noteDao.setNoteDeleted();
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> setNoteUndeleted() async {
+    try {
+      return await _appDatabase.noteDao.setNoteUndeleted();
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> unselectAllNotes() async {
+    try {
+      return await _appDatabase.noteDao.unselectAllNotes();
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<String?> getSortType() async {
+    try {
+      if (await _appDatabase.sortDao.getSortType() != null) {
+        return _appDatabase.sortDao.getSortType();
+      } else {
+        insertSort(SortModel(id: 1, sortType: 'title'));
+        return await _appDatabase.sortDao.getSortType();
+      }
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> insertSort(SortModel sortModel) async {
+    try {
+      return await _appDatabase.sortDao.insertSort(sortModel);
+    } catch (e) {
+      throw DatabaseException();
+    }
+  }
+
+  @override
+  Future<void> selectFavoriteNote(int id) async {
+    try {
+      return await _appDatabase.noteDao.selectFavoriteNote(id);
+    } catch (e) {
+      throw DatabaseException();
+    }
   }
 }
